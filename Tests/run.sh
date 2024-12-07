e5b7a7b8b2f64327f36abb8429eec780fb04353b
kli63 <kli63@ThisIsLaptop>
2024-12-07 13:32:18 -0500

    minor pybind changes

--- Tests/run.sh
+++ Tests/run.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Get the current Python executable
+# Get the current Python executable and force CMake to use it
 PYTHON_EXE=$(which python3)
+echo "Using Python: $PYTHON_EXE"
 
 # Clean previous build
 rm -rf build
 mkdir build
 cd build
 
-# Export Python paths for CMake to find
-export Python3_ROOT_DIR=$(dirname $(dirname $PYTHON_EXE))
-
-# Configure and build
-cmake ..
+# Configure and build with forced Python path
+CMAKE_PREFIX_PATH=$(dirname $(dirname $PYTHON_EXE)) cmake -DPython3_EXECUTABLE=$PYTHON_EXE ..
 make
 
 # Run the test with the same Python
